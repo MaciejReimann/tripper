e605fca6c8ebbcf50c9097c91d45521dcc82f7a4
Maciej Reimann <maciej.reimann@gmail.com>
2023-11-29 10:14:09 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import * as React from "react";
+
+import trips from "./trips/trips.json";
+
+/*
+ * Requirements:
+ * ● The website should be responsive and built with ReactJS functional components and Typescript
+ * ● ChakraUI should be used for UI components
+ * ● Data should be downloaded asynchronously from a mock server - serving json files (any technology)
+ * ● Details of a trip should be pulled from a separate single-trip endpoint
+ * ● New cards should be loaded on page scroll (infinite scrolling)
+ * ● Data from the server should be cached and preserved between pages
+ * ● The code should be placed in any repository along with instructions on how to run the project
+ * ● [Nice to have] Unit tests
+ */
 
 function App() {
-  const [count, setCount] = useState(0);
+  console.log(trips);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <>Trips</>;
 }
 
 export default App;
